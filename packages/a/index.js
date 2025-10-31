20fa87d8bc65cac687b1c254c1cf0004e8b0bed2
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2025-10-31 13:19:47 -0400

    Update runtime tests.

--- packages/a/index.js
+++ packages/a/index.js
@@ -1,3 +1,9 @@
 // Importing this will import react as a side-effect. We've added console.log statements to all react packages that get installed, so you can see which one ends up being used.
-import { useFloating } from "@floating-ui/react-dom";
-console.log(useFloating);
+
+// Testing out an import that is actually declared in a/package.json
+import { Anchor } from "@radix-ui/react-popper";
+console.log(Anchor);
+
+// Another test to see what happens if we import directly from @floating-ui/react-dom (which isn't directly declared in any package.json)
+// import { useFloating } from "@floating-ui/react-dom";
+// console.log(useFloating);
